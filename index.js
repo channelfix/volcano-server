7ae24b02010a146164de564910e2fcea23ef9e84
Arnelle Balane <arnellebalane@gmail.com>
2018-07-05 19:55:46 +0800

    handle set event in backend

--- index.js
+++ index.js
@@ -21,9 +21,30 @@ const ws = websockets({
     path: '/ws'
 });
 
+let db = {};
+
 ws.on('connection', socket => {
-    socket.on('test', data => {
-        console.log(data);
-        socket.emit('test', 'hello from server');
+    socket.emit('connect', 'You are now connected to the volcano server.');
+
+    socket.on('set', ({path, value}) => {
+        const segments = path.split('/');
+        const target = getObject(db, segments.slice(0, segments.length - 1));
+        target[segments[segments.length - 1]] = value;
+
+        socket.emit('change', db);
     });
 });
+
+function getObject(source, segments) {
+    if (!segments.length) {
+        return source;
+    }
+
+    const first = segments[0];
+    console.log(first);
+     if (!source.hasOwnProperty(first)) {
+        source[first] = {};
+    }
+
+    return getObject(source[first], segments.slice(1));
+}
